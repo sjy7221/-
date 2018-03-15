b8a6b149f722187d2a3caf2549d68661842f58cc
shijunyi <313703436@qq.com>
2018-03-15 16:51:56 +0800

    1

---
+++ server/src/app/Controllers/RoomController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\Controllers;
+
+use app\Models\AppModel;
+use Server\CoreBase\Controller;
+
+/**
+ * Created by PhpStorm.
+ * User: zhangjincheng
+ * Date: 16-7-15
+ * Time: 下午3:51
+ */
+class AppController extends Controller
+{
+    /**
+     * @var AppModel
+     */
+    public $AppModel;
+
+    protected function initialization($controller_name, $method_name)
+    {
+        parent::initialization($controller_name, $method_name);
+        $this->AppModel = $this->loader->model('AppModel', $this);
+    }
+
+   public function jinru()
+   {
+    print_r(111);
+   }
+}
