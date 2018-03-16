79b18439fe90dbd451e75ca6ce15c84276343cad
shijunyi <313703436@qq.com>
2018-03-16 16:12:38 +0800

    1

--- server/src/app/Controllers/RoomController.php
+++ server/src/app/Controllers/RoomController.php
@@ -77,16 +77,33 @@ class RoomController extends Controller
 
        }else{
          $this->roomInfo = $re['roomInfo'];
-         var_dump($this->roomInfo);
+        
         $this->sendToUids($this->uids,['game_go','游戏开始'],false);
-        $this->game();
+        $this->fapai($re['roomInfo']['guize']['renshu']);
        }
        $this->destroy();
 
     }
 
-    public function game()
+    public function fapai($renshu)
     {
-       echo 1;
+       //开局人数
+        if($renshu == 2 || $renshu == 3){
+
+        $pai = [31,32,33,34,41,42,43,44,51,52,53,54,61,62,63,64,71,72,73,74,81,82,83,84,91,92,93,94,101,102,103,104,111,112,113,114,121,122,123,124,131,132,133,134,141,142,143,160];
+        shuffle($pai);
+
+        $numb = count($pai)/$people;
+        $pais = [];
+
+        for($i = 0;$i<$people;$i++){
+            for($j=0;$j<$numb;$j++){
+               $pais[$i][] =  array_pop($pai);
+            }
+            sort($pais[$i]);
+        }
+        var_dump ($pais);
+        }
+       
     }
 }
