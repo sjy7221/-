80c083dffe2b7c9a413d2bdd9e1d430a75b624a2
shijunyi <313703436@qq.com>
2018-03-16 15:15:04 +0800

    1

--- server/src/app/Controllers/RoomController.php
+++ server/src/app/Controllers/RoomController.php
@@ -64,6 +64,7 @@ class RoomController extends Controller
             $this->close();
             return;
         }
+        $this->send($this->mid,$this->roomInfo);
         //模型处理数据
         $re = yield $this->CommModel->jinru($this->mid, $this->room_id, $this->roomInfo);
        
