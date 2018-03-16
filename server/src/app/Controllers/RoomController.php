de31ba2c51a01fdacb9120634950f441656a73a7
shijunyi <313703436@qq.com>
2018-03-16 15:10:24 +0800

    1

--- server/src/app/Controllers/RoomController.php
+++ server/src/app/Controllers/RoomController.php
@@ -54,11 +54,11 @@ class RoomController extends Controller
 
     public function jinru()
     {
-        var_dump($this->roomInfo);
+
       if ($this->is_destroy) {
             return;
         }
-        var_dump($this->uids);
+ 
           if (!in_array($this->mid,$this->uids)  && count($this->uids) >= $this->roomInfo['guize']['renshu']) {
             $this->send('人数已满', false);
             $this->close();
