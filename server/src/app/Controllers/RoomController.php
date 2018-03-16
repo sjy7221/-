ad739009e0267e25f3fafdb1363eb524563f6707
shijunyi <313703436@qq.com>
2018-03-16 09:35:48 +0800

    1

--- server/src/app/Controllers/RoomController.php
+++ server/src/app/Controllers/RoomController.php
@@ -22,7 +22,7 @@ class RoomController extends Controller
     {
         parent::initialization($controller_name, $method_name);
         $this->AppModel = $this->loader->model('AppModel', $this);
-      print_r($controller.'/'.$method_name.':数据为'. $this->client_data->data);
+      var_dump($controller.'/'.$method_name.':数据为'. $this->client_data->data);
     }
 
     public function jinru()
