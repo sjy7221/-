95e8a4563521b648d3f43c7f6c9a7e51b4dbf5cf
shijunyi <313703436@qq.com>
2018-03-19 11:06:20 +0800

    1

--- server/src/app/Models/CommModel.php
+++ server/src/app/Models/CommModel.php
@@ -26,7 +26,7 @@ class CommModel extends Model
     }
     public function jinru($mid,$room_id,$roominfo)
     {
-    	echo $mid .'OKOK';
+    
     	 if (in_array($mid, $roominfo['weizhi'])){
     	 	return false;
     	 }else{ 
@@ -60,7 +60,7 @@ class CommModel extends Model
                 ->where('id', $mid)
                 ->coroutineSend();
             yield $this->redis_pool->getCoroutine()->hset('uids_'.$room_id,$mid,1);
-            // yield $this->redis_pool->getCoroutine()->hset($room_id,'userInfo'=>$userinfo);
+             yield $this->redis_pool->getCoroutine()->hset($room_id,'userInfo',serialize($userInfo));
     	 }
     	 
             if(count($userinfo['users']) ==  $roominfo['guize']['renshu']){
