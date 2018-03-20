1a37995e573468d80a5bb4553087ff2442dad3e3
shijunyi <313703436@qq.com>
2018-03-20 09:47:21 +0800

    3

--- server/Game.php
+++ server/Game.php
@@ -82,7 +82,7 @@
     //连对
     function liandui($pai)
     {
-        return 'aaaaaaaaaa';
+       
             $numb =  zhuanhuan($pai);
             //判断打出去的牌是不是炸弹
         if(count($numb) == 4 && $numb[0] == $numb[1] && $numb[1] == $numb[2] && $numb[2] == $numb[3]){
