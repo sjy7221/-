d283cacbe75780015b2d35a50bcae27de4376568
shijunyi <313703436@qq.com>
2018-03-20 10:26:40 +0800

    4

--- server/src/app/Controllers/RoomController.php
+++ server/src/app/Controllers/RoomController.php
@@ -196,7 +196,7 @@ class RoomController extends Controller
    
                $leix =  liandui($pai); 
              }
-          var_dump($leix);
+         var_dump($this->gameInfo);
           
        
     }
