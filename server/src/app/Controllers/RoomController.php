537459c72711b231a34643f376bd2f48f2218100
shijunyi <313703436@qq.com>
2018-03-23 12:56:04 +0800

    2

--- server/src/app/Controllers/RoomController.php
+++ server/src/app/Controllers/RoomController.php
@@ -71,7 +71,7 @@ class RoomController extends Controller
      */
     public function jinru()
     {
-        var_dump($this->roomInfo);
+        var_dump($this->userInfo);
 //        echo  "【jinru】".json_encode($this->data). "\n";
 //      if ($this->is_destroy) {
 //            return;
